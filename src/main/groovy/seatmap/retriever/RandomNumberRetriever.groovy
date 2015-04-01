e8099db9c352f4bd6f5a42a87fee35b35324cff6
Benjamin Rosenberger (ext.rob) <benjamin.rosenberger@catalysts.cc>
2015-04-01 18:19:19 +0200

    adding random generator to index output

--- src/main/groovy/seatmap/retriever/RandomNumberRetriever.groovy
+++ src/main/groovy/seatmap/retriever/RandomNumberRetriever.groovy
@@ -2,7 +2,9 @@ package seatmap.retriever
 
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
+import org.springframework.stereotype.Service
 
+@Service
 class RandomNumberRetriever {
     public int retrieveNewSeedNumber(String uri) {
         Document doc = Jsoup.connect(uri).get()
--- src/main/java/seatmap/controller/AppController.java
+++ src/main/java/seatmap/controller/AppController.java
@@ -1,13 +1,29 @@
 package seatmap.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import seatmap.Application;
+import seatmap.retriever.RandomNumberRetriever;
 
 @Controller
 public class AppController {
+    private final RandomNumberRetriever randomGenerator;
+
+    @Autowired
+    public AppController(RandomNumberRetriever randomGenerator) {
+        this.randomGenerator = randomGenerator;
+    }
 
     @RequestMapping(value = "/")
-    public String index() {
-        return "index";
+    public ModelAndView index() {
+        final Map<String, Object> model = new HashMap<>();
+        model.put("randomNumber", randomGenerator.retrieveNewSeedNumber(Application.RANDOM_INT_URL));
+        return new ModelAndView("index", model);
     }
 }
