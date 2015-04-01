d4e8aaf4bc7a6e7b91c85d4a7018daa620c4292f
Benjamin Rosenberger (ext.rob) <benjamin.rosenberger@catalysts.cc>
2015-04-01 18:13:15 +0200

    add first thymeleaf page

--- src/main/java/seatmap/Application.java
+++ src/main/java/seatmap/Application.java
@@ -1,10 +1,12 @@
-import seatmap.retriever.RandomNumberRetriever;
+package seatmap;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class Application {
     public static final String RANDOM_INT_URL = "https://www.random.org/integers/?num=1&min=1&max=100000&col=5&base=10&format=html&rnd=new&format=plain";
 
     public static void main(String... args) {
-        System.out.println("new random number retrieved: "
-                + new RandomNumberRetriever().retrieveNewSeedNumber(RANDOM_INT_URL));
+        SpringApplication.run(Application.class, args);
     }
 }
